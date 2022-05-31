c26f825028bff169c935c76768b92315abb21f50
ryuki62 <ryukiwon001@gmail.com>
2022-05-31 16:26:01 +0900

    kiwon snack and kiosk vo work

--- src/MC/VO/KioskVO.java
+++ src/MC/VO/KioskVO.java
@@ -4,33 +4,40 @@ public class KioskVO {
 	private int knum;
 	private String kiosk_bill;
 	private int count;
+
 	public KioskVO() {
 		super();
 	}
+
 	public KioskVO(int knum, String kiosk_bill, int count) {
 		super();
 		this.knum = knum;
 		this.kiosk_bill = kiosk_bill;
 		this.count = count;
 	}
+
 	public int getKnum() {
 		return knum;
 	}
+
 	public void setKnum(int knum) {
 		this.knum = knum;
 	}
+
 	public String getKiosk_bill() {
 		return kiosk_bill;
 	}
+
 	public void setKiosk_bill(String kiosk_bill) {
 		this.kiosk_bill = kiosk_bill;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
-	
-	
+
 }
--- src/MC/VO/SnacksAndSideVO.java
+++ src/MC/VO/SnacksAndSideVO.java
@@ -1,66 +1,79 @@
 package MC.VO;
 
 public class SnacksAndSideVO {
-	
+
 	private String snacksandside_name;
 	private int price;
 	private int setprice;
 	private int ssize;
 	private int popularity;
 	private int reorder;
-	
+
 	public SnacksAndSideVO() {
 		super();
 	}
+
 	public String getSnacksandside_name() {
 		return snacksandside_name;
 	}
+
 	public void setSnacksandside_name(String snacksandside_name) {
 		this.snacksandside_name = snacksandside_name;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getSetprice() {
 		return setprice;
 	}
+
 	public void setSetprice(int setprice) {
 		this.setprice = setprice;
 	}
+
 	public int getSsize() {
 		return ssize;
 	}
+
 	public void setSsize(int ssize) {
 		this.ssize = ssize;
 	}
+
 	public int getPopularity() {
 		return popularity;
 	}
+
 	public void setPopularity(int popularity) {
 		this.popularity = popularity;
 	}
+
 	public int getReorder() {
 		return reorder;
 	}
+
 	public void setReorder(int reorder) {
 		this.reorder = reorder;
 	}
+
 	@Override
 	public String toString() {
-		String tmp1,tmp2;
-		if(popularity==1) {
+		String tmp1, tmp2;
+		if (popularity == 1) {
 			tmp1 = "¢¾";
-		} else tmp1 = " ";
-		
-		if(reorder==1) {
+		} else
+			tmp1 = " ";
+
+		if (reorder == 1) {
 			tmp2 = "R";
-		} else tmp2 = " ";
-		return "[" + tmp1 + "]" + "[" + tmp2 +"] " + 
-			snacksandside_name + "\t\t" + price + "¿ø";
+		} else
+			tmp2 = " ";
+		return "[" + tmp1 + "]" + "[" + tmp2 + "] " + snacksandside_name + "\t\t" + price + "¿ø";
 	}
-	
-	
+
 }
