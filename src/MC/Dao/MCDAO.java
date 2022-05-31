97f59bca4b8f8946161ac283ce9afd90c1584496
ryuki62 <ryukiwon001@gmail.com>
2022-05-31 16:48:23 +0900

    kiwon dao work

--- src/MC/Dao/MCDAO.java
+++ src/MC/Dao/MCDAO.java
@@ -1,17 +1,49 @@
 package MC.Dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import MC.VO.DessertVO;
 import MC.VO.DrinkVO;
 import MC.VO.HamburgerVO;
+import MC.VO.SetVO;
+import MC.VO.SnacksAndSideVO;
 
 public class MCDAO {
 	SqlSessionFactory factory = MybatisConfig.getSqlSessionFactory();
-	
-	//디저트 검색
-	private DessertVO SearchDDessert(String str) {
+
+	// 세트만들기
+	public SetVO BugerSet(HamburgerVO Hamburger, String str) {
+
+		return null;
+	}
+
+	// 해피밀
+	public SetVO HappyMeal() {
+
+		return null;
+	}
+
+	// 주문하기
+	public List<SetVO> order() {
+
+		return null;
+	}
+
+	// 장바구니
+	public List<SetVO> shopping_basket(Object obj, int count) {
+
+		return null;
+	}
+
+	// 취소
+	public void cancel() {
+
+	}
+
+	public DessertVO SearchDDessert(String str) {
 		SqlSession session = null;
 		session = factory.openSession();
 		MCMapper mapper = session.getMapper(MCMapper.class);
@@ -19,9 +51,7 @@ public class MCDAO {
 		DessertVO result = mapper.SearchDDessert(str);
 		return result;
 	}
-	
-	
-	//음료 검색
+
 	public DrinkVO SearchDrink(String str) {
 		SqlSession session = null;
 		session = factory.openSession();
@@ -30,15 +60,53 @@ public class MCDAO {
 		DrinkVO result = mapper.SearchDrink(str);
 		return result;
 	}
-	
-	
-	//햄버거 검색
+
 	public HamburgerVO SearchHamburger(String str) {
 		SqlSession session = null;
 		session = factory.openSession();
 		MCMapper mapper = session.getMapper(MCMapper.class);
-		
+
 		HamburgerVO result = mapper.SearchHamburger(str);
 		return result;
+	}
+
+	public SnacksAndSideVO SearchSnacksAndSide(String str) {
+
+		return null;
+	}
+
+	public List<HamburgerVO> getHamburger() {
+
+		return null;
+	}
+
+	public List<SnacksAndSideVO> getMSnaksandSides() {
+
+		return null;
+	}
+
+	public List<SnacksAndSideVO> getLSnaksandSides() {
+
+		return null;
+	}
+
+	public List<SnacksAndSideVO> getSDrink() {
+
+		return null;
+	}
+
+	public List<SnacksAndSideVO> getMDrink() {
+
+		return null;
+	}
+
+	public List<SnacksAndSideVO> getLDrink() {
+
+		return null;
+	}
+
+	public List<SnacksAndSideVO> getSauce() {
+
+		return null;
 	}
 }
